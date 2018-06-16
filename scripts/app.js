271bd5f17ca850269c06a7fc8a6c3db62383ec1b
Joseph Park <joseph.thomas.park@gmail.com>
2018-06-16 16:14:01 +1000

    Add marker to map

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,13 @@
-var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 2
-        });
-    }
+function initMap() {
+
+    
+    const location = {lat: -34.397, lng: 150.644};
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+    // center: {lat: -34.397, lng: 150.644},
+    center: location,
+    zoom: 2
+    });
+
+    const marker = new google.maps.Marker({position: location, map: map});
+}
