abf2c8a43814b8a360ebf6d0804c575d1745296b
Joseph Park <joseph.thomas.park@gmail.com>
2018-06-16 16:50:34 +1000

    Update map

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,9 @@
 function initMap() {
-
-    
     const location = {lat: -34.397, lng: 150.644};
-
     const map = new google.maps.Map(document.getElementById('map'), {
     // center: {lat: -34.397, lng: 150.644},
     center: location,
     zoom: 2
     });
-
     const marker = new google.maps.Marker({position: location, map: map});
 }
