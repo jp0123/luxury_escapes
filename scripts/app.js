35c35fe0c41d6ca6b44caac3b6ca5237d8c33eb2
Joseph Park <joseph.thomas.park@gmail.com>
2018-06-18 15:26:44 +1000

    Update VP section to show destination image + add comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,23 @@
+// Google Maps
 const api_latitude = [];
 const api_longitude = [];
 const destination_coordinates = [];
+// VP section
 const destination_title =[];
 const destination_description = [];
 const destination_location = [];
 const destination_country = [];
 const destination_price = [];
 const destination_accommodation = [];
+// Button
 const details_button = [];
 const details_button_p1 = "https://luxuryescapes.com/au/offer/";
 const details_button_p2 = [];
 const details_button_p3 = [];
+// Image
+const destination_images = [];
+const destination_image_p1 = "https://res.cloudinary.com/lux-group/image/upload/f_auto,fl_progressive,q_auto:eco,c_fill,g_auto,ar_16:9/"
+const destination_image_p2 = [];
 
 // Fetch LuxGroup API
 const api = fetch('https://api.luxgroup.com/api/public-offers')
@@ -20,10 +27,12 @@ const api = fetch('https://api.luxgroup.com/api/public-offers')
   .then(function(myJson) {
     let destinations_array = myJson.result.length;
     for(let i = 0; i < destinations_array; i++) {
+        // Google Maps
         let latitude = myJson.result[i].lowest_price_package.property.latitude;
         let longitude = myJson.result[i].lowest_price_package.property.longitude;
         api_latitude.push(latitude);
         api_longitude.push(longitude);
+        // VP section
         destination_coordinates.push([latitude, longitude]);
         destination_title.push(myJson.result[i].name);
         destination_description.push(myJson.result[i].description);     
@@ -31,10 +40,15 @@ const api = fetch('https://api.luxgroup.com/api/public-offers')
         destination_location.push(myJson.result[i].location_heading);
         destination_price.push(myJson.result[i].lowest_price_package.price);
         destination_accommodation.push(myJson.result[i].lowest_price_package.property.name);
+        // Button
         details_button_p2.push(myJson.result[i].slug);
         details_button_p3.push(myJson.result[i].id_salesforce_external);
         let details_button_link = `${details_button_p1}${details_button_p2[i]}/${details_button_p3[i]}`;
         details_button.push(details_button_link);
+        // Images
+        destination_image_p2.push(myJson.result[i].images[0].id_cloudinary_external);
+        let destination_image = `${destination_image_p1}${destination_image_p2[i]}`
+        destination_images.push(destination_image);
     }
     // Run the function after the promise has been fulfilled.
     initMap();
@@ -47,9 +61,6 @@ function initMap() {
         center: location,
         zoom: 2
         });
-
-    // This is a marker to show the center of the map (i.e. Sydney is the central point).
-    const marker = new google.maps.Marker({position: location, map: map});
 
     // Places a marker on each of the destinations listed in the API.
     for(let i = 0; i < destination_coordinates.length; i++) {
@@ -67,6 +78,7 @@ function initMap() {
       let destinationPrice = destination_price[i];
       let destinationAccommodation = destination_accommodation[i];
       let destinationDetailsButton = details_button[i];
+      let destinationImage = destination_images[i];
 
       let infoWindow = new google.maps.InfoWindow({
           content: destinationTitle
@@ -75,18 +87,18 @@ function initMap() {
       marker.addListener('click', function() {
         infoWindow.open(map, marker);
 
-        markerOffer(destinationTitle, destinationDescription, destinationCountry, destinationLocation, destinationPrice, destinationAccommodation, destinationDetailsButton);
+        markerOffer(destinationTitle, destinationDescription, destinationCountry, destinationLocation, destinationPrice, destinationAccommodation, destinationDetailsButton, destinationImage);
         });
     }
   }
 
-const markerOffer = (title, description, country, location, price, accommodation, detailsButton) => {
+const markerOffer = (title, description, country, location, price, accommodation, detailsButton, destinationImage) => {
   $('.vp-title').text(title);
   $('.vp-description').text(description);
   $('.vp-country').text(country);
   $('.vp-location').text(location);
   $('.vp-price-1').text(`$${price} AU`);
   $('.vp-accommodation').text(accommodation);
-  $('.vp-button').attr("href", detailsButton);
-
+  $('.vp-button').attr('href', detailsButton);
+  $('#offer-image').attr('src', destinationImage);
 }
